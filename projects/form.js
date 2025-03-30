e824d17d36616ff61929d98ac258e886ac52a027
ayuningtyaskartikas <ayuningtyaskartikas@gmail.com>
2025-03-30 00:51:06 -1000

    yes

--- projects/form.js
+++ projects/form.js
@@ -1,29 +1,10 @@
 document.getElementById("myform").addEventListener('submit',function(event) {
-
-    const formData = {
-
-    };
-    console.log(formData);
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "submit.json", true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            document.getElementById('message').innerHTML = response.message;
-            document.getElementById('myForm').innerHTML = "";
-        } else if (xhr.readyState === 4) {
-            alert('Error submitting form.');
-        }
-    };
-    xhr.send(JSON.stringify(formData));
-
     event.preventDefault();
     //alert("Form submitted");
-    const firstname = document.getElementById("fname").value;
-    console.log(firstname);
-    const lastname = document.getElementById("lname").value;
-    console.log(lastname);
+    const firstN = document.getElementById("fname").value;
+    console.log(firstN);
+    const lastN = document.getElementById("lname").value;
+    console.log(lastN);
     const email = document.getElementById("email").value;
     console.log(email);
     const extension = document.getElementById("extension").value;
@@ -48,5 +29,32 @@ document.getElementById("myform").addEventListener('submit',function(event) {
         alert("You need to be at least 18.");
         return;
     }
+
+    const formData = {
+        fname: firstN,
+        lname: lastN,
+        email: email,
+        extension: extension,
+        phone: phone,
+        datebirth: datebirth,
+        password: password,
+        country: country,
+        pokemon: pokemon
+    }
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "submit.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            document.getElementById('message').innerHTML = response.message;
+            document.getElementById('myForm').innerHTML = "";
+        } else if (xhr.readyState === 4) {
+            alert('Error submitting form.');
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+    console.log(formData);
     
 });
